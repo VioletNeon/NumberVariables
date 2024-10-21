2bead5546c518cb5347b260ea64a5720eb4e3707
Куликов Валерий Романович <VRKulikov@beeline.ru>
2024-10-21 23:39:25 +0300

    Task-1: Added type variants initialization

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int characters = 177;
+        System.out.println("Значение переменной characters с типом int равно " + characters);
+
+        byte apples = 15;
+        System.out.println("Значение переменной apples с типом byte равно " + apples);
+
+        short cities = 23_456;
+        System.out.println("Значение переменной cities с типом short равно " + cities);
+
+        long stars = 3_759_231_937L;
+        System.out.println("Значение переменной stars с типом long равно " + stars);
+
+        float liters = 154.565f;
+        System.out.println("Значение переменной liters с типом float равно " + liters);
+
+        double lengthDifference = 1_789.565_232;
+        System.out.println("Значение переменной lengthDifference с типом double равно " + lengthDifference);
     }
 }
