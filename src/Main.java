d7f44a12d11cc52eb07b8f230cf6bf33c6ea7544
Куликов Валерий Романович <VRKulikov@beeline.ru>
2024-10-22 08:59:39 +0300

    Task-2: Initialized variables with various type values

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int characters = 177;
+        int characters = 1_767_088_132;
         System.out.println("Значение переменной characters с типом int равно " + characters);
 
         byte apples = 15;
@@ -17,5 +17,23 @@ public class Main {
 
         double lengthDifference = 1_789.565_232;
         System.out.println("Значение переменной lengthDifference с типом double равно " + lengthDifference);
+
+        float degrees = 27.12f;
+        long grainsOfSand = 987_678_965_549L;
+        float depthLevel = 2.786f;
+        short blocks = 569;
+        short xCoordinate = -159;
+        short employees = 27_897;
+        byte mathTeachers = 67;
+
+        System.out.println("Инициализированы переменные со значениями и типами:\n" +
+                "degrees с типом float равно " + degrees + ";\n" +
+                "grainsOfSand с типом long равно " + grainsOfSand + ";\n" +
+                "depthLevel с типом float равно " + depthLevel + ";\n" +
+                "blocks с типом short равно " + blocks + ";\n" +
+                "xCoordinate с типом short равно " + xCoordinate + ";\n" +
+                "employees с типом short равно " + employees + ";\n" +
+                "mathTeachers с типом byte равно " + mathTeachers + ";"
+        );
     }
 }
